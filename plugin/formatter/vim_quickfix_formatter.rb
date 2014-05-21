15b72a6dd171f8ff9ce3143833ce70f3396f5e93
John Gallagher <john@synapticmishap.co.uk>
2014-05-21 05:13:59 +0100

    Added quickfix formatter for vim.

---
+++ plugin/formatter/vim_quickfix_formatter.rb
@@ -0,0 +1,32 @@
+require 'rspec/core/formatters/base_text_formatter'
+
+module RSpec
+  module Core
+    module Formatters
+      class VimQuickfixFormatter < BaseTextFormatter
+        def example_failed(example)
+          message = example.execution_result[:exception].message
+          backtrace = example.execution_result[:exception].backtrace
+          output.puts "#{extract_path(backtrace)}: [FAIL] #{flatten(message)}"
+        end
+
+        def dump_summary(duration, example_count, failure_count, pending_count)
+        end
+
+        def seed(number)
+        end
+
+        def flatten(message)
+          (message.split("\n").map { |part| part.strip }).join(" ")
+        end
+        private :flatten
+
+        def extract_path(backtrace)
+          calling_file = backtrace.find { |line| line =~ /^(.*_spec\.rb:.*):in/ }
+          $1
+        end
+        private :extract_path
+      end
+    end
+  end
+end
